a9b223162738cf4ce1ce9fa4e7ac461f2f71b62d
bhatshakran <bhatshakran@gmail.com>
2021-05-24 18:21:12 +0530

    Form connected to db

--- admin/categories.php
+++ admin/categories.php
@@ -22,7 +22,42 @@
                         
                         
                         <div class="col-xs-6">
-                            <form action="">
+                           
+                           <?php
+                            
+                            if(isset($_POST['submit'])) {
+                                $cat_title = $_POST['cat_title'];
+                                
+                                if($cat_title ==  "" || empty($cat_title)) {
+                                    
+                                    echo "This field should not be empty";
+                                    
+                                }else {
+                                    
+                                    
+                                    
+                                    $query =  "INSERT INTO categories(cat_title) ";
+                                    $query .= "VALUE('{$cat_title}') ";
+                                    
+                                    $create_category_query = mysqli_query($connection, $query);
+                                    
+                                    
+                                    if(!$create_category_query) {
+                                        
+                                        die("QUERY FAILED" . mysqli_error($connection));
+                                        
+                                    }
+                                    
+                                    
+                                }
+                            }
+                            
+                            
+                            ?>
+                           
+                           
+                           
+                            <form action="categories.php" method="post">
                                 <div class="form-group">
                                    
                                    <label for="cat_title">
