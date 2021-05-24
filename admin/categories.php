430f3509b9dff8a05ff6483c46f66dd38b2b161b
bhatshakran <bhatshakran@gmail.com>
2021-05-24 17:57:46 +0530

    Table added

--- admin/categories.php
+++ admin/categories.php
@@ -38,6 +38,24 @@
                                 </div>
                             </form>
                         </div>
+                        
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Cricket Category</td>
+                                         
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                        
                     </div>
                 </div>
