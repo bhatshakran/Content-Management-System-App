58a3a10f99c1c5faf367f7295141882863d593ad
bhatshakran <bhatshakran@gmail.com>
2021-05-24 17:26:30 +0530

    Some code refactoring done in admin dashboard files

---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+ </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
