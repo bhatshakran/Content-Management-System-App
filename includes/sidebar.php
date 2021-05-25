30db157004da8e5c81c900b7b152542755c96bc6
bhatshakran <bhatshakran@gmail.com>
2021-05-25 11:58:10 +0530

    Category page working added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -33,9 +33,10 @@
                                 
                                 
                                 while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                                    $cat_id = $row['id'];
                                     $cat_title = $row['cat_title'];
                                     
-                                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                                    echo "<li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
                                 }
                                 
                                 ?>
