fa02e34db310b71a29f557d3cfd421a3fc536290
bhatshakran <bhatshakran@gmail.com>
2021-05-25 11:45:00 +0530

    Created  a post page

--- index.php
+++ index.php
@@ -21,17 +21,17 @@
                 
                 <?php 
                 
-                
                 $query = "SELECT * FROM posts";
                 $select_all_posts_query = mysqli_query($connection, $query);
                     
                     while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        
+                        $post_id = $row['post_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_content = $row['post_content'];
                         $post_image = $row['post_image'];
                         $post_date = $row['post_date'];
+                    
                       ?>
                          <h1 class="page-header">
                     Page Heading
@@ -40,14 +40,14 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?> "><?php echo $post_title?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image;?> " alt="image">
                 <hr>
                 <p><?php echo $post_content?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
