a750c6f5755aa3d2ac9636d32900bf6a4e629a40
bhatshakran <bhatshakran@gmail.com>
2021-05-25 06:29:00 +0530

    Post form integrated with backend

---
+++ admin/includes/add_posts.php
@@ -0,0 +1,72 @@
+<?php
+
+
+if(isset($_POST['create_post'])) {
+    
+    $post_title = $_POST['title'];
+        
+        $post_author = $_POST['author'];                           
+                            
+        $post_category_id = $_POST['post_category_id'];                             
+        $post_status = $_POST['post_status'];                             
+        $post_image = $_FILES['image']['name'];
+ 
+        $post_image_temp = $_FILES['image']['tmp_name']; 
+    
+        $post_tags = $_POST['post_tags'];
+    
+      $post_content = $_POST['post_content'];
+    
+        $post_comment_count = 4;   
+    
+        $post_date = date('d-m-y'); 
+    
+
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+    
+}
+
+
+
+
+?>
+  
+
+  
+  
+  <form action="" method="post" enctype="multipart/form-data">
+   
+    <div class="form-group">
+    <label for="title">Post Title</label>
+    <input type="text" class="form-control" name="title">
+    </div>
+    <div class="form-group">
+    <label for="post_category">Post Category Id</label>
+    <input type="text" class="form-control" name="post_category_id">
+    </div>
+    <div class="form-group">
+    <label for="">Post Author</label>
+    <input type="text" class="form-control" name="author">
+    </div>
+    <div class="form-group">
+    <label for="post_tags">Post Tags</label>
+    <input type="text" class="form-control" name="post_tags">
+    </div>
+    <div class="form-group">
+    <label for="post_image">Post Image</label>
+    <input type="file" name="image">
+    <div class="form-group">
+    <label for="post_status">Post Status</label>
+    <input type="text" class="form-control" name="post_status">
+    </div>
+    <div class="form-group">
+    <label for="post_content">Post Content</label>
+        <textarea  class="form-control" name="post_content" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form_group">
+    <input type="submit" value="Publish Post" name="create_post" class="btn btn-primary">
+    </div>
+    </div>
+    
+</form>
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -35,7 +35,7 @@
                         <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                        <a href="posts.php" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
                                 <a href="#">View All Post </a>
@@ -46,7 +46,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                        <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                    
                     <li class="active">
