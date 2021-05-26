ceb8d56a7705ef8451f91440c15e3c67e6f84829
shaqran_bhat <bhatshakran@gmail.com>
2021-05-26 12:59:12 +0530

    Introduced google charts itno dashboard

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -62,6 +62,8 @@ if(!isset($_SESSION['role'])) {header("Location: /cms/index.php");}
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  
 
    
 
