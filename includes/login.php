d863a40119a457bf543c17f0d4b9c6c0c5fb38a7
shaqran_bhat <bhatshakran@gmail.com>
2021-05-26 10:24:57 +0530

    Form validation added

--- includes/login.php
+++ includes/login.php
@@ -33,7 +33,27 @@ if(isset($_POST['login'])) {
     
     
     while($row = mysqli_fetch_array($select_user_query)) {
-        echo $db_id = $row['user_id'];
+        
+
+         $db_user_id = $row['user_id'];
+         $db_username = $row['username'];
+         $db_user_firstname = $row['user_firstname'];
+         $db_user_lastname = $row['user_lastname'];
+         $db_user_password = $row['user_password'];
+         $db_user_role = $row['user_role'];
+     
+    }
+    
+    
+    
+    if($username !== $db_username && $password !== $db_user_password) {
+        header("Location: ../index.php");
+//        echo "<h1>Invalid Credentials</h1>";
+        
+    } else if($username === $db_username && $password == $db_user_password) {
+        header("Location: ../admin/index.php");
+    }else {
+         header("Location: ../index.php");
     }
 
 
