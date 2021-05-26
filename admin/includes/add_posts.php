b1e0ea32698e984c1fb70067a63ce958aa1c917e
shaqran_bhat <bhatshakran@gmail.com>
2021-05-26 05:54:44 +0530

    created users table in db

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -17,8 +17,7 @@ if(isset($_POST['create_post'])) {
         $post_tags = $_POST['post_tags'];
     
       $post_content = $_POST['post_content'];
-    
-//        $post_comment_count = 4;   
+     
     
         $post_date = date('d-m-y'); 
     
@@ -27,7 +26,7 @@ if(isset($_POST['create_post'])) {
     move_uploaded_file($post_image_temp, "../images/$post_image");
     
     
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)";
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
     
     $query .= "VALUES({$post_category_id}, '{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}' )";
     
