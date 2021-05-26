f58089e701f082e0708e86a4afd72da7b50323e7
shaqran_bhat <bhatshakran@gmail.com>
2021-05-26 11:57:43 +0530

    User profile added

--- admin/profile.php
+++ admin/profile.php
@@ -6,7 +6,26 @@
 
 if(isset($_SESSION['username'])) {
     
-    echo $_SESSION['username'];
+    $username =  $_SESSION['username'];
+
+    
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    
+    $select_user_profile = mysqli_query($connection, $query);
+    
+    
+    while($row = mysqli_fetch_array($select_user_profile)) {
+
+    $user_id =  $row['user_id'];    
+    $user_email =  $row['user_email'];
+    $user_firstname =  $row['user_firstname'];
+    $user_lastname =  $row['user_lastname'];
+    $user_password =  $row['user_password'];
+    $user_image =  $row['user_image'];
+    $user_role =  $row['user_role'];
+   
+        
+    }
 }
 
 
@@ -33,7 +52,59 @@ if(isset($_SESSION['username'])) {
                             <small>Author</small>
                         </h1>
                         
-  
+                              <form action="" method="post" class="form">
+                                  <div class="form-group">
+                                     <label for="username">
+                                         Username
+                                     </label>
+                                      <input type="text" class="form-control"
+                                      name="username"
+                                      value="<?php echo $username?>">
+                                  </div>
+                                  
+                                  <div class="form-group">
+                                     <label for="username">
+                                         Firstname
+                                     </label>
+                                      <input type="text" class="form-control"
+                                      name="user_firstname"
+                                      value="<?php echo $user_firstname?>">
+                                  </div>
+                                  
+                                  <div class="form-group">
+                                     <label for="username">
+                                         Lastname
+                                     </label>
+                                      <input type="text" class="form-control"
+                                      name="user_lastname"
+                                      value="<?php echo $user_lastname?>">
+                                  </div>
+                                  
+                                  <div class="form-group">
+                                     <label for="username">
+                                         Email
+                                     </label>
+                                      <input type="email" class="form-control"
+                                      name="user_email"
+                                      value="<?php echo $user_email?>">
+                                      
+                                      
+                                      
+                                      <div class="form-group">
+                                     <label for="username">
+                                         Password
+                                     </label>
+                                      <input type="password" class="form-control"
+                                      name="user_password"
+                                      value="<?php echo $user_password?>">
+                                  </div>
+                                  </div>
+                                  
+                                  <div class="form-group">
+                                      <button class="btn btn-primary" type="submit" name='update_user'>Update User</button>
+                                  </div>
+                                  
+                              </form>
                        
                     </div>
                 </div>
