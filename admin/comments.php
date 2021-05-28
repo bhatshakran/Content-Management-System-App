982a91f0ad612330f754bfd228e4f368c40387ff
shaqran_bhat <bhatshakran@gmail.com>
2021-05-28 14:49:14 +0530

    Comments styled

--- admin/comments.php
+++ admin/comments.php
@@ -3,23 +3,13 @@
     
 
 
-    <div id="wrapper">
+    <div class="container mx-auto text-center">
 
         <!-- Navigation -->
         <?php include "includes/admin_navigation.php";
         ?>
 
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                    <h1 class="page-header">
-                           Welcome to Admin
-                            <small>Author</small>
-                        </h1>
+        
                         
 <?php
                         
@@ -53,18 +43,9 @@ if(isset($_GET['source'])) {
                    
                        
                     </div>
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <!-- /#page-wrapper -->
+               
 
    <?php include "includes/admin_footer.php";
    ?>
 
-</body>
 
-</html>
--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -1,16 +1,16 @@
-     <table class="table table-bordered table-hover">
-                            <thead>
+     <table class="mt-20 mb-8 extrasmall md:table-auto glass md:mr-auto">
+                            <thead class="border-b">
                                 <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Comment</th>
-                                    <th>Email</th>
-                                    <th>Status</th>
-                                    <th>In response to</th>
-                                      <th>Date</th>
-                                    <th>Approve</th>
-                                    <th>Unapprove</th>
-                                    <th>Delete</th>
+                                    <!-- <th class="tabledata">Id</th> -->
+                                    <!-- <th class="tabledata">Author</th> -->
+                                    <th class="tabledata">Comment</th>
+                                    <th class="tabledata">Email</th>
+                                    <th class="tabledata">Status</th>
+                                    <th class="tabledata">In response to</th>
+                                      <!-- <th class="tabledata">Date</th> -->
+                                    <th class="tabledata">Approve</th>
+                                    <th class="tabledata">Unapprove</th>
+                                    <th class="tabledata">Delete</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -27,14 +27,14 @@
         $comment_email = $row['comment_email'];                             
         $comment_content = $row['comment_content'];                             
         $comment_status = $row['comment_status'];                             
-        $comment_date = $row['comment_date'];                             
+        // $comment_date = $row['comment_date'];                             
          
         echo "<tr>";
-        echo "<td>$comment_id</td>";
-        echo "<td>$comment_author</td>";
-        echo "<td>$comment_content</td>";
+        // echo "<td class='tabledata'>$comment_id</td>";
+        // echo "<td class='tabledata'>$comment_author</td>";
+        echo "<td class='tabledata'>$comment_content</td>";
       
-//        $query = "SELECT * FROM categories WHERE comment_id = {$post_category_id}";
+//        $query = "SELECT * FROM c class="tabledata"ategories WHERE comment_id = {$post_category_id}";
 //        
 //        $select_categories_id = mysqli_query($connection, $query);
 //        
@@ -45,8 +45,8 @@
 //        }
         
         
-        echo "<td>$comment_email</td>";
-        echo "<td>$comment_status</td>";
+        echo "<td class='tabledata'>$comment_email</td>";
+        echo "<td class='capitalize tabledata'>$comment_status</td>";
         
         $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
         
@@ -57,17 +57,17 @@
             $post_id = $row['post_id'];
             $post_title = $row['post_title'];
             
-             echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
+             echo "<td class='tabledata'><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
         
         }
         
         
         
        
-        echo "<td>$comment_date</td>";
-         echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
-        echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
-        echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
+        // echo "<td class='tabledata'>$comment_date</td>";
+         echo "<td class='text-green-400 tabledata'><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+        echo "<td class='text-indigo-600 tabledata'><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
+        echo "<td class='text-red-600 tabledata'><a href='comments.php?delete=$comment_id'>Delete</a></td>";
         echo "</tr>";
             
             
