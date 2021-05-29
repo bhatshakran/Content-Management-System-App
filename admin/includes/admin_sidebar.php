aa43731ecd73f7867b40459d54c3c92be31ff28a
shaqran_bhat <bhatshakran@gmail.com>
2021-05-29 16:40:00 +0530

    modded login

--- admin/includes/admin_sidebar.php
+++ admin/includes/admin_sidebar.php
@@ -19,7 +19,7 @@
                                 <a href="posts.php">View All Post </a>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                 <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
                                 </svg>
                             </li>
                             <li class="flex items-center ml-6 text-sm text-blue-400">
