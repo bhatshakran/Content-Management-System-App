6a0cbe72c3e25af15dcd56dc91803ed6c51bd27d
shaqran_bhat <bhatshakran@gmail.com>
2021-05-29 06:50:40 +0530

    modified navigation bug

--- includes/navigation.php
+++ includes/navigation.php
@@ -9,10 +9,10 @@
 
 
 
-    <div class="flex-row w-full md:grid md:grid-cols-3">
+    <div class="flex-row w-full md:flex md:justify-between ">
 
 
-    <div id="home" class="flex items-center justify-between ml-6 text-blue-600 md:ml-4 md:w-1/3">
+    <div id="home" class="flex items-center justify-between ml-6 text-blue-600 md:ml-4 ">
    
     
    
@@ -31,8 +31,8 @@
     </div>
     </div>
 
-    <div id="links" class="hidden w-full border-t-4 panel-lg md:h-auto md:block md:w-2/3 md:border-0 md:shadow-none">
-    <ul class="block py-2 md:flex ">
+    <div id="links" class="hidden w-full border-t-4 panel-lg md:h-auto md:block md:border-0 md:shadow-none md:w-max ">
+    <ul class="block py-2 md:py-0 md:flex ">
 
     <?php
                     
@@ -43,7 +43,7 @@
                         
                         $cat_title = $row['cat_title'];
                         
-                        echo "<li class='py-4 nav-links'><a class='text-gray-500 ' href='#'>{$cat_title}</a></li>";
+                        echo "<li class='py-4 md:py nav-links '><a class='text-gray-500 ' href='#'>{$cat_title}</a></li>";
                     }
                     
                     
@@ -52,11 +52,11 @@
                 ?>
                  <li class='flex items-center justify-center text-gray-500 nav-links'>
                  <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
                         </svg>
                         </div>
-                        <a class='py-4 text-gray-500 md:mr-2' href="admin/index.php">Admin</a>
+                        <a class='py-4 text-gray-500 md:py md:mr-2 ' href="admin/index.php">Admin</a>
                     </li>
     </ul>
     </div>
--- index.php
+++ index.php
@@ -57,8 +57,10 @@
                 <!-- date -->
                 <p class="text-xs font-thin text-gray-500"><span class="glyphicon glyphicon-time"></span> <?php echo $post_date?></p>
                <!-- image -->
+               <a href="">
                 <img class="w-24 h-24 mt-2 mb-3" src="images/<?php echo $post_image;?> " alt="image">
-               <!-- content -->
+                </a>
+                <!-- content -->
                 <p class="text-sm font-light tracking-wider text-justify"><?php echo $post_content?></p>
                 <!-- Read more -->
                 <a class="text-blue-400 hover:text-blue-800" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
